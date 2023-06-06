83b89dd2f54362dcf9390addca88bfe9e656c20a
labib3013 <alfarabyhd30@gmail.com>
2023-06-06 20:22:54 +0700

    add : test

--- app/models/test.js
+++ app/models/test.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class crud_test extends Model {
+  class test extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,15 +11,15 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      
     }
   }
-  crud_test.init({
-    name: DataTypes.STRING,
-    number: DataTypes.INTEGER
+  test.init({
+    nama: DataTypes.STRING,
+    deskripsi: DataTypes.TEXT,
+    nomor: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'crud_test',
+    modelName: 'test',
   });
-  return crud_test;
+  return test;
 };
---
+++ migrations/20230605104204-create-test.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tests', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nama: {
+        type: Sequelize.STRING
+      },
+      deskripsi: {
+        type: Sequelize.TEXT
+      },
+      nomor: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tests');
+  }
+};
